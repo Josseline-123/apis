84f0f0851e75f349b2f4c1465c1d842cc827a3ff
Josseline-123 <josselinefg32@gmail.com>
2024-07-26 22:39:47 -0400

    first comm

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,26 @@
 let data;
-async function obtenerDatos(){
- const res = await fetch('https://mindicador.cl/api/');
-  data = await res.json();
+async function obtenerDatos() {
+    const res = await fetch('https://mindicador.cl/api/');
+    data = await res.json();
 
 }
 
 const convertirValor = function () {
-   const valor = document.querySelector('#valor').value;
-   const moneda = document.querySelector('#moneda').value;
-try
-{
-   const valorConvertido = valor / data[moneda].valor;
+    const valor = document.querySelector('#valor').value;
+    const moneda = document.querySelector('#moneda').value;
 
-   document.querySelector('#resultado').texContent=`El valor convertido es:
+    console.log("data:" + data)
+    console.log("data{moneda}"+ data[moneda])
+    console.log("data{moneda}.valor"+ data[moneda].valor)
+    try {
+        const valorConvertido = valor / data[moneda].valor;
+        console.log('valorConvertido: '+ valorConvertido)
+
+        document.querySelector('#resultado').textContent = `El valor convertido es:
     ${valorConvertido.toFixed(2)}`;
+    }
+    catch (error) {
+        console.log('Hubo un error en la conversion')
+    }
 }
-catch(error){
-    console.log ('Hubo un error en la conversion')
-}
-}
-    obtenerDatos();
+obtenerDatos();
